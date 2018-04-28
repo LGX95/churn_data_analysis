365936b79de941d8373b311e4ee776a8c9fba3e0
LGX95 <ligengxin95@gmail.com>
2018-04-28 19:53:04 +0800

    plot sorted total charges

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import numpy as np
+import pandas as pd
 from flask import Flask, render_template
-from pyecharts import Line
+from pyecharts import Bar
 
 
 app = Flask(__name__)
@@ -11,19 +12,31 @@ REMOTE_HOST = "https://cdn.bootcss.com/echarts/4.0.4/"
 
 @app.route('/')
 def index():
-    line = relu()
+    bar = get_bar()
     return render_template('index.html',
-                           myechart=line.render_embed(),
+                           myechart=bar.render_embed(),
                            host=REMOTE_HOST,
-                           script_list=line.get_js_dependencies())
+                           script_list=bar.get_js_dependencies())
 
 
-def relu():
-    x = np.arange(-5, 6, 1)
-    y = np.maximum(0, x)
-    line = Line('ReLU')
-    line.add('', x, y, )
-    return line
+def get_total_charges():
+    customer_df = pd.read_csv(
+        '~/Downloads/WA_Fn-UseC_-Telco-Customer-Churn.csv')
+    total_charges = customer_df['TotalCharges']
+    total_charges = total_charges.replace(' ', np.nan)
+    total_charges.dropna(inplace=True)
+    total_charges = total_charges.map(float)
+    total_charges = total_charges.sort_values(ascending=False)
+    return total_charges
+
+
+def get_bar():
+    total_charges = get_total_charges()
+    bar = Bar('Total Charges')
+    bar.add('Total Charges', total_charges.index, total_charges,
+            xaxis_name='customer id', is_datazoom_show=True,
+            datazoom_type='both', datazoom_range=[0, 75])
+    return bar
 
 
 if __name__ == '__main__':
