cabaf520e42e885e6702a3bf732d77356aae6c60
LGX95 <ligengxin95@gmail.com>
2018-04-28 19:13:40 +0800

    pyecharts example

--- run.py
+++ run.py
@@ -1,12 +1,29 @@
+import numpy as np
 from flask import Flask, render_template
+from pyecharts import Line
 
 
 app = Flask(__name__)
 
 
+REMOTE_HOST = "https://cdn.bootcss.com/echarts/4.0.4/"
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    line = relu()
+    return render_template('index.html',
+                           myechart=line.render_embed(),
+                           host=REMOTE_HOST,
+                           script_list=line.get_js_dependencies())
+
+
+def relu():
+    x = np.arange(-5, 6, 1)
+    y = np.maximum(0, x)
+    line = Line('ReLU')
+    line.add('', x, y, )
+    return line
 
 
 if __name__ == '__main__':
