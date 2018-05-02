77fe182afb7701b06a72bdce72429b92140e313f
LGX95 <ligengxin95@gmail.com>
2018-05-02 08:37:42 +0800

    use Data in run.py

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
-import numpy as np
-import pandas as pd
 from flask import Flask, render_template
 from pyecharts import Bar, Line, Overlap
+
+from Data import Data
 
 
 app = Flask(__name__)
 
 
 REMOTE_HOST = "https://cdn.bootcss.com/echarts/4.0.4/"
+
+
+data = Data('~/Downloads/WA_Fn-UseC_-Telco-Customer-Churn.csv')
 
 
 @app.route('/')
@@ -19,30 +22,14 @@ def index():
                            script_list=graph.get_js_dependencies())
 
 
-def get_data():
-    """读取csv文件信息，返回 total_charges 即其累计比例
-
-    Return:
-        total_charges: 从大到小排序的 TotalCharges Series
-    """
-    customer_df = pd.read_csv(
-        '~/Downloads/WA_Fn-UseC_-Telco-Customer-Churn.csv')
-    total_charges = customer_df['TotalCharges']
-    total_charges = total_charges.replace(' ', np.nan)
-    total_charges.dropna(inplace=True)
-    total_charges = total_charges.map(float)
-    total_charges = total_charges.sort_values(ascending=False)
-    charges_proportion = total_charges.cumsum() / sum(total_charges) * 100
-    return total_charges, charges_proportion
-
-
 def get_graph():
     """得到 Total Charges 的图
 
     return:
         overlap: Overlap 实例，添加了柱状图和折线图
     """
-    total_charges, charges_proportion = get_data()
+    total_charges, charges_proportion = \
+        data.get_sorted_and_proportion('TotalCharges')
     # 区域缩放的选项，同时用于 bar 和 line
     dataroom_opt = {
         'is_datazoom_show': True,
