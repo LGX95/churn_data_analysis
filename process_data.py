f5c4fa50f5d0a40e31c2d55b1775aac41906d9d6
LGX95 <ligengxin95@gmail.com>
2018-05-15 15:18:17 +0800

    tsne X

--- process_data.py
+++ process_data.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 from kmodes.kprototypes import KPrototypes
+from sklearn.manifold import TSNE
+
+from Data import Data
 
 
 data_root = '~/Downloads/WA_Fn-UseC_-Telco-Customer-Churn.csv'
@@ -28,3 +31,11 @@ if __name__ == '__main__':
     # sort cluster result
     sorted_clusters = sort_clusters(clusters)
     np.save('sorted_clusters.npy', arr=sorted_clusters)
+
+    # tsne X
+    print('\nStarting tsne X...')
+    data = Data(data_root)
+    X_num = data.df_num
+    tsne = TSNE(n_components=2, init='pca', random_state=0)
+    X_tsne = tsne.fit_transform(X_num)
+    np.save('X_tsne.npy', arr=X_tsne)
